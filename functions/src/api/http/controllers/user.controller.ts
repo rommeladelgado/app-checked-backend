c476ff0c6e85939eb9249ff4f75a355509198e9d
Rommel Delgado <rommel.delgado.github@gmail.com>
2025-09-20 17:16:20 -0600

    fix controllers

--- functions/src/api/http/controllers/user.controller.ts
+++ functions/src/api/http/controllers/user.controller.ts
@@ -1,17 +1,19 @@
-import {UserRepositoryImpl} from "../../../modules/users/infrastructure/user-repository-impl";
+import {UserRepositoryImpl} from "@src/modules/users/infrastructure/user-repository-impl";
 import {NextFunction, Response, Request} from "express";
-import {RegisterUserUseCase} from "../../../modules/users/application/register-user.use-case";
-import {LoginUserUseCase} from "../../../modules/users/application/login-user.use-case";
+import {RegisterUserUseCase} from "@src/modules/users/application/register-user.use-case";
+import {LoginUserUseCase} from "@src/modules/users/application/login-user.use-case";
 
 const userRepository = new UserRepositoryImpl();
-
 async function register(req: Request, res: Response, next: NextFunction) {
   try {
     const {email, password} = req.body;
-    const loginUserUseCase = new RegisterUserUseCase(userRepository);
-    const authResponse = await loginUserUseCase.execute({email, password});
-    res.json(authResponse);
-  } catch (e) {
+    const registerUserUseCase = new RegisterUserUseCase(userRepository);
+    const authResponse = await registerUserUseCase.execute({email, password});
+    res.status(201).json(authResponse);
+  } catch (e: any) {
+    if (e.message === "User already exists") {
+      return res.status(409).json({error: e.message});
+    }
     next(e);
   }
 }
@@ -21,13 +23,13 @@ async function login(req: Request, res: Response, next: NextFunction) {
     const {email, password} = req.body;
     const loginUserUseCase = new LoginUserUseCase(userRepository);
     const authResponse = await loginUserUseCase.execute({email, password});
-    res.json(authResponse);
-  } catch (e) {
+    res.status(200).json(authResponse);
+  } catch (e: any) {
+    if (e.message === "Invalid credentials") {
+      return res.status(401).json({error: e.message});
+    }
     next(e);
   }
 }
 
-export {
-  register,
-  login,
-};
+export {register, login};
